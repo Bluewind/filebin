4cf80cd7cc9a8cdfb357e6fdca7ed4e05e806b31
Florian Pritz <bluewind@server-speed.net>
2011-01-28 22:59:37 +0100

    fix checking for $msg variable before using
    Signed-off-by: Florian Pritz <bluewind@server-speed.net>

--- application/views/file/delete_form.php
+++ application/views/file/delete_form.php
@@ -1,7 +1,7 @@
 <div style="text-align:center">
   <?php echo form_open('file/delete/'.$id); ?>
 		<p>
-			<?php if($msg) echo $msg."<br />"; ?>
+			<?php if(isset($msg)) echo $msg."<br />"; ?>
 			Password:<input type="password" name="password" size="10" />
 			<input type="submit" value="Delete" name="process" />
     </p>
