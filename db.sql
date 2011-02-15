b7dfe097725ef4f9163ebc2c8ef092480a5d74d9
Florian Pritz <bluewind@server-speed.net>
2011-02-15 17:57:34 +0100

    update db.sql
    Signed-off-by: Florian Pritz <bluewind@server-speed.net>

--- db.sql
+++ db.sql
@@ -1,12 +1,12 @@
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 
 CREATE TABLE IF NOT EXISTS `files` (
-  `hash` varchar(32) COLLATE utf8_bin NOT NULL,
-  `id` varchar(8) COLLATE utf8_bin NOT NULL,
+  `hash` varchar(32) CHARACTER SET ascii NOT NULL,
+  `id` varchar(6) CHARACTER SET ascii COLLATE ascii_bin NOT NULL,
   `filename` varchar(256) COLLATE utf8_bin NOT NULL,
-  `password` varchar(40) COLLATE utf8_bin DEFAULT NULL,
-  `date` int(11) NOT NULL,
-  `mimetype` varchar(255) COLLATE utf8_bin NOT NULL,
+  `password` varchar(40) CHARACTER SET ascii COLLATE ascii_bin DEFAULT NULL,
+  `date` int(11) unsigned NOT NULL,
+  `mimetype` varchar(255) CHARACTER SET ascii NOT NULL,
   PRIMARY KEY (`id`),
   KEY `date` (`date`),
   KEY `hash` (`hash`,`id`)
