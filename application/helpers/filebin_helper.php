d8f426985a3f7443af59d23bbf1dff5e586c428e
Florian Pritz <bluewind@server-speed.net>
2011-05-28 20:00:30 +0200

    use correct suffixes for sizes
    Signed-off-by: Florian Pritz <bluewind@server-speed.net>

--- application/helpers/filebin_helper.php
+++ application/helpers/filebin_helper.php
@@ -3,7 +3,7 @@
 // Source: http://ibnuyahya.com/format-bytes-to-kb-mb-gb/
 function format_bytes($size, $precision = 2){
 		$base = log($size) / log(1024);
-		$suffixes = array('B', 'kB', 'MB', 'GB', 'TB' , 'PB' , 'EB');
+		$suffixes = array('B', 'KiB', 'MiB', 'GiB', 'TiB' , 'PiB' , 'EiB');
 		return round(pow(1024, $base - floor($base)), $precision) . $suffixes[floor($base)];
 }
 
