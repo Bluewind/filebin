b11b171bc8054d0734176527d80415227502a37d
Florian Pritz <bluewind@xinu.at>
2011-12-17 11:27:34 +0100

    add short features description to upload_form
    Signed-off-by: Florian Pritz <bluewind@xinu.at>

--- application/views/file/upload_form.php
+++ application/views/file/upload_form.php
@@ -91,8 +91,20 @@ document.write('\
 <p>Uploads/pastes are deleted after <?php echo $upload_max_age; ?> days<?php if($small_upload_size > 0): ?>
   unless they are smaller than <?php echo format_bytes($small_upload_size); ?>
   <?php endif; ?>. Maximum upload size is <?php echo format_bytes($max_upload_size); ?></p>
+<p><h2>Features</h2></p>
 <p>For shell uploading/pasting and download information for the client go to <a href="<?php echo site_url("file/client"); ?>"><?php echo site_url("file/client"); ?></a></p>
-<p>You can use the <?php echo anchor("file/upload_history", "history"); ?> to find old uploads using your password.</p>
+<p>You can use the <?php echo anchor("file/upload_history", "history"); ?> to find old uploads using the password supplied when creating the upload.</p>
+<p>How to link your pastes:</p>
+<ul>
+	<li><span class="example">/&lt;ID&gt;/</span> automatically highlight the paste</li>
+	<li><span class="example">/&lt;ID&gt;</span> set the detected MIME type and let the browser do the rest</li>
+	<li><span class="example">/&lt;ID&gt;/plain</span> force the MIME type to be text/plain</li>
+	<li><span class="example">/&lt;ID&gt;/&lt;file extension&gt;</span> override auto detection and use the supplied file extension or language name for highlighting</li>
+	<li><span class="example">/&lt;ID&gt;/qr</span> display a qr code containing a link to <span class="example">/&lt;ID&gt;/</span></li>
+	<li><span class="example">/&lt;ID&gt;/rmd</span> convert markdown to HTML</li>
+	<li><span class="example">/&lt;ID&gt;/ascii</span> convert text with ANSI (shell) escape codes to HTML</li>
+</ul>
+<p>If your upload is not detected as text, only <span class="example">/&lt;ID&gt;/qr</span> will work as above and all others will simply return the file with the detected MIME type.</p>
 <br />
 <?php if($contact_me_url) {?><p>If you experience any problems feel free to <a href="<?php echo $contact_me_url; ?>">contact me</a>.</p>
 <br /><?php }; ?>
