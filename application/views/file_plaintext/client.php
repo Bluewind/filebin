b256550f2b2640d4ff507d5c759251bac538bbd9
Florian Pritz <bluewind@xinu.at>
2012-04-05 15:11:19 +0200

    improve start page for cli clients
    Signed-off-by: Florian Pritz <bluewind@xinu.at>

--- application/views/file_plaintext/client.php
+++ application/views/file_plaintext/client.php
@@ -1,12 +1,13 @@
-Shell:
-  curl -n -F "file=@/home/user/foo" <?php echo site_url(); ?>   (binary safe)
-  cat file | curl -n -F "file=@-;filename=stdin" <?php echo site_url(); ?>   (binary safe)
+Shell (binary safe):
+  curl -n -F "file=@/home/user/foo" <?php echo site_url()."\n"; ?>
+  cat file | curl -n -F "file=@-;filename=stdin" <?php echo site_url()."\n"; ?>
 
 Client:
 Development (git): http://git.server-speed.net/users/flo/fb
 Latest release: <?php echo $client_link."\n"; ?>
 GPG sigs, older versions: <?php echo $client_link_dir."\n"; ?>
 
-If you want to use authentication (needed for deleting) add the following to your ~/.netrc:
+If you want to use authentication (needed for deleting) add the following 
+to your ~/.netrc:
   machine paste.xinu.at password my_secret_password
 
--- application/views/file_plaintext/upload_form.php
+++ application/views/file_plaintext/upload_form.php
@@ -1,2 +1,17 @@
 Uploads/pastes are deleted after <?php echo $upload_max_age; ?> days<?php if($small_upload_size > 0): ?> unless they are smaller than <?php echo format_bytes($small_upload_size); ?><?php endif; ?>.
 Maximum upload size is <?php echo format_bytes($max_upload_size); ?>.
+
+How to link your uploads:
+ - "/<ID>/" automatically highlight the uploads
+ - "/<ID>" set the detected MIME type and let the browser do the rest
+ - "/<ID>/plain" force the MIME type to be text/plain
+ - "/<ID>/<file extension>" override auto detection and use the supplied
+   file extension or language name for highlighting
+ - "/<ID>/qr" display a qr code containing a link to /<ID>/
+ - "/<ID>/rmd" convert markdown to HTML
+ - "/<ID>/ascii" convert text with ANSI (shell) escape codes to HTML
+
+If your upload is not detected as text, only "/<ID>/qr" and "/<ID>/plain"
+will work as above and all others will simply return the file with the
+detected MIME type.
+
