dcf0caba66ddf03109fe874e07e6750d54547b3a
Florian Pritz <bluewind@xinu.at>
2012-04-09 00:29:32 +0200

    Don't show delete button when not found
    Signed-off-by: Florian Pritz <bluewind@xinu.at>

--- application/views/file/delete_form.php
+++ application/views/file/delete_form.php
@@ -1,31 +1,31 @@
 <div style="text-align:center">
   <?php echo form_open('file/delete/'.$id); ?>
 			<?php if(isset($msg)) echo "<p>".$msg."</p>"; ?>
-			<p>You are about to delete the following upload:</p>
-			<table style="margin: auto">
 			<?php if($filedata): ?>
-				<tr>
-					<td class="title">ID</td>
-					<td class="text"><a href="<?php echo site_url($id); ?>/"><?php echo $id; ?></a></td>
-				</tr>
-				<tr>
-					<td class="title">Filename</td>
-					<td class="text"><?php echo $filedata["filename"]; ?></td>
-				</tr>
-				<tr>
-					<td class="title">Date of upload</td>
-					<td class="text"><?php echo date("r", $filedata["date"]); ?></td>
-				</tr>
-				<tr>
-					<td class="title">Size</td>
-					<td class="text"><?php echo format_bytes($filedata["size"]); ?></td>
-				</tr>
-				<tr>
-					<td class="title">Mimetype</td>
-					<td class="text"><?php echo $filedata["mimetype"]; ?></td>
-				</tr>
-			<?php endif; ?>
-			</table>
-		<input type="submit" value="Delete" name="process" />
+				<p>You are about to delete the following upload:</p>
+				<table style="margin: auto">
+					<tr>
+						<td class="title">ID</td>
+						<td class="text"><a href="<?php echo site_url($id); ?>/"><?php echo $id; ?></a></td>
+					</tr>
+					<tr>
+						<td class="title">Filename</td>
+						<td class="text"><?php echo $filedata["filename"]; ?></td>
+					</tr>
+					<tr>
+						<td class="title">Date of upload</td>
+						<td class="text"><?php echo date("r", $filedata["date"]); ?></td>
+					</tr>
+					<tr>
+						<td class="title">Size</td>
+						<td class="text"><?php echo format_bytes($filedata["size"]); ?></td>
+					</tr>
+					<tr>
+						<td class="title">Mimetype</td>
+						<td class="text"><?php echo $filedata["mimetype"]; ?></td>
+					</tr>
+				</table>
+			<input type="submit" value="Delete" name="process" />
+		<?php endif; ?>
   </form>
 </div>
