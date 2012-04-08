aac0fb938069a89f8befed640fabef6d84879fc8
Florian Pritz <bluewind@xinu.at>
2012-04-09 00:40:05 +0200

    Fix netrc information on client page.
    Signed-off-by: Florian Pritz <bluewind@xinu.at>

--- application/views/file/client.php
+++ application/views/file/client.php
@@ -8,9 +8,9 @@ cat file | curl -n -F "file=@-;filename=stdin" <?php echo site_url(); ?>   (bina
 <?php if($client_link) {?>Latest release: <a href="<?php echo $client_link; ?>"><?php echo $client_link; ?></a>.<br /><?php }; ?>
 GPG sigs, older versions: <a href="<?php echo $client_link_dir; ?>"><?php echo $client_link_dir; ?></a>
 </p>
-<p>If you want to use authentication (needed for deleting) add the following to your ~/.netrc:</p>
+<p>To authenticate add the following to your ~/.netrc:</p>
 <pre>
-machine paste.xinu.at password my_secret_password
+machine paste.xinu.at login my_username password my_secret_password
 </pre>
 <p><b>Packages:</b><br />
 Arch Linux: pacman -S fb-client<br />
