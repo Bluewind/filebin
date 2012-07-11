700fc306106c7acebb73e9479f93006388ea80a0
Florian Pritz <bluewind@xinu.at>
2012-07-12 01:08:15 +0200

    Add unique index for users.username
    Signed-off-by: Florian Pritz <bluewind@xinu.at>

---
+++ application/migrations/006_add_username_index.php
@@ -0,0 +1,21 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Migration_Add_username_index extends CI_Migration {
+
+	public function up()
+	{
+		$this->db->query("
+			ALTER TABLE `users`
+			ADD UNIQUE `username` (`username`);
+		");
+	}
+
+	public function down()
+	{
+		$this->db->query("
+			ALTER TABLE `files`
+			DROP INDEX `username`;
+		");
+	}
+}
