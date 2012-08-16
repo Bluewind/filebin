6904300a38c995dd46b3ce1ca2bc5fbafdde8cff
Florian Pritz <bluewind@xinu.at>
2012-08-16 16:30:30 +0200

    html_header: fix indentation
    Signed-off-by: Florian Pritz <bluewind@xinu.at>

--- application/views/file/html_header.php
+++ application/views/file/html_header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
   <head>
-    <title><?php echo $title; ?></title>
+	<title><?php echo $title; ?></title>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 	<link rel="stylesheet" type="text/css" href="<?php echo link_with_mtime("/data/paste.css"); ?>" />
 <?php if (file_exists(FCPATH."/data/paste-$current_highlight.css")) {?>
@@ -9,20 +9,20 @@
 <?php } ?>
   </head>
   <body>
-    <div class="top_bar">
-      <a class="raw_link no" href="<?php echo site_url(); ?>">New</a> |
-      <a class="raw_link no" href="<?php echo site_url($id); ?>">Raw</a> |
-      <a class="raw_link no" href="<?php echo site_url($id."/plain"); ?>">Plain</a> |
-      <a class="raw_link no" href="<?php echo site_url($id."/info"); ?>">Info</a> |
-      Currently: <?php echo $current_highlight; ?> |
-      Timeout: <?php echo $timeout; ?>
-      <div style="float:right;">
-        <a class="raw_link no" href="<?php echo site_url($id)."/"; ?>">Code</a> |
-        <a class="raw_link no" href="<?php echo site_url($id."/rmd"); ?>">Render Markdown</a>
-      </div>
-    </div>
-  <script type="text/javascript">
-		/* <![CDATA[ */
+	<div class="top_bar">
+	  <a class="raw_link no" href="<?php echo site_url(); ?>">New</a> |
+	  <a class="raw_link no" href="<?php echo site_url($id); ?>">Raw</a> |
+	  <a class="raw_link no" href="<?php echo site_url($id."/plain"); ?>">Plain</a> |
+	  <a class="raw_link no" href="<?php echo site_url($id."/info"); ?>">Info</a> |
+	  Currently: <?php echo $current_highlight; ?> |
+	  Timeout: <?php echo $timeout; ?>
+	  <div style="float:right;">
+		<a class="raw_link no" href="<?php echo site_url($id)."/"; ?>">Code</a> |
+		<a class="raw_link no" href="<?php echo site_url($id."/rmd"); ?>">Render Markdown</a>
+	  </div>
+	</div>
+<script type="text/javascript">
+/* <![CDATA[ */
 function update_anchor_highlight() {
 	var anchor = window.location.hash.substr(1);
 	var element = document.getElementById("highlight_line");
@@ -48,7 +48,7 @@ if ("onhashchange" in window) {
 		update_anchor_highlight();
 	}
 }
-    /* ]]> */
-  </script>
-    <table class="content">
-      <tr>
+/* ]]> */
+</script>
+	<table class="content">
+	  <tr>
