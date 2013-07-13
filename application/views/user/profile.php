2eac725505a74ab246168fde34f607446473d896
Florian Pritz <bluewind@xinu.at>
2013-07-14 00:04:20 +0200

    Improve help text for upload id limits
    Signed-off-by: Florian Pritz <bluewind@xinu.at>

--- application/views/user/profile.php
+++ application/views/user/profile.php
@@ -20,7 +20,7 @@
 		<label class="control-label" for="inputUploadIDLimits">Upload ID length limits</label>
 		<div class="controls">
 			<input type="text" id="inputUploadIDLimits" name="upload_id_limits" placeholder="number-number" value="<?php echo $profile_data["upload_id_limits"]; ?>">
-			<span class="help-block">&lt;number&gt;-&lt;number&gt; (min: 3, max: 64; example: "5-9")</span>
+			<span class="help-block">Values have to be between 3 and 64 inclusive. Please remember that longer IDs don't protect your pastes from being found if you post the link somewhere a search enginge can see it.</span>
 		</div>
 	</div>
 
