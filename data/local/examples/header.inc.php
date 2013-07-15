9fcff61dc31d54b7855d0e5d928de615164bddb4
Florian Pritz <bluewind@xinu.at>
2013-07-15 21:15:58 +0200

    header.inc example: Clarify include position
    Well a little bit at least. Use the source if you want to know more...
    
    Signed-off-by: Florian Pritz <bluewind@xinu.at>

--- data/local/examples/header.inc.php
+++ data/local/examples/header.inc.php
@@ -1,4 +1,4 @@
 <!--
-This header will be included right after the <body> tag.
+This header will be included (kind of) right after the <body> tag.
 Use it to prepend your own site's navigation if needed.
 -->
