9d94ea077812c1b0736b9374645c50eadafa94ae
Markus Cisler <m@kuchen.io>
2013-09-22 14:44:46 +0200

    fix paste view on mobile devices

--- application/views/file/upload_history_thumbnails.php
+++ application/views/file/upload_history_thumbnails.php
@@ -2,7 +2,7 @@
 	<?php echo form_open("file/do_delete/", array("id" => "delete_form", "style" => "display: inline")); ?>
 		<button class="btn btn-danger" id="delete_button" style="display: none">Delete selected</button>
 	</form>
-	<button class="btn" id="toggle_delete_mode" style="display: inline">Delete mode</button>
+	<button class="btn btn-default" id="toggle_delete_mode" style="display: inline">Delete mode</button>
 </div>
 
 <?php include 'nav_history.php'; ?>
