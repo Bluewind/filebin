4340ea3039362867aba32b173b212e762e293f80
Markus Cisler <m@kuchen.io>
2013-09-25 12:38:32 +0200

    Make tables responsive

--- application/views/file/file_info.php
+++ application/views/file/file_info.php
@@ -1,30 +1,32 @@
 <div class="center">
 	<?php if($filedata): ?>
-		<table class="table" style="margin: auto">
-			<tr>
-				<td class="title">ID</td>
-				<td class="text"><a href="<?php echo site_url($id); ?>/"><?php echo $id; ?></a></td>
-			</tr>
-			<tr>
-				<td class="title">Filename</td>
-				<td class="text"><?php echo htmlspecialchars($filedata["filename"]); ?></td>
-			</tr>
-			<tr>
-				<td class="title">Date of upload</td>
-				<td class="text"><?php echo date("r", $filedata["date"]); ?></td>
-			</tr>
-			<tr>
-				<td class="title">Date of removal</td>
-				<td class="text"><?php echo $timeout; ?></td>
-			</tr>
-			<tr>
-				<td class="title">Size</td>
-				<td class="text"><?php echo format_bytes($filedata["filesize"]); ?></td>
-			</tr>
-			<tr>
-				<td class="title">Mimetype</td>
-				<td class="text"><?php echo $filedata["mimetype"]; ?></td>
-			</tr>
-		</table>
+		<div class="table-responive">
+			<table class="table" style="margin: auto">
+				<tr>
+					<td class="title">ID</td>
+					<td class="text"><a href="<?php echo site_url($id); ?>/"><?php echo $id; ?></a></td>
+				</tr>
+				<tr>
+					<td class="title">Filename</td>
+					<td class="text"><?php echo htmlspecialchars($filedata["filename"]); ?></td>
+				</tr>
+				<tr>
+					<td class="title">Date of upload</td>
+					<td class="text"><?php echo date("r", $filedata["date"]); ?></td>
+				</tr>
+				<tr>
+					<td class="title">Date of removal</td>
+					<td class="text"><?php echo $timeout; ?></td>
+				</tr>
+				<tr>
+					<td class="title">Size</td>
+					<td class="text"><?php echo format_bytes($filedata["filesize"]); ?></td>
+				</tr>
+				<tr>
+					<td class="title">Mimetype</td>
+					<td class="text"><?php echo $filedata["mimetype"]; ?></td>
+				</tr>
+			</table>
+		</div>
 	<?php endif; ?>
 </div>
