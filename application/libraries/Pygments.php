Bluewind/filebin

commit a11e06d4906c2e1186779b42d5e87a17f66c50ea

Fix mime2lexer failing when using pygments info
Signed-off-by: Florian Pritz <bluewind@xinu.at>

application/libraries/Pygments.php
@@ -21,7 +21,7 @@ class Pygments {
 	}
 
 	private static function get_pygments_info() {
-		return cache_function_full('pygments_info', 1800, function() {
+		return cache_function_full('pygments_info-v2', 1800, function() {
 			ob_start();
 			passthru("python ".escapeshellarg(FCPATH."scripts/get_lexer_list.py"));
 			$output = ob_get_clean();
@@ -148,7 +148,7 @@ class Pygments {
 		// fall back to pygments own list if not found in our list
 		foreach (self::get_pygments_info() as $lexer) {
 			if (isset($lexer['mimetypes'][$this->mimetype])) {
-				return $lexer;
+				return $lexer['names'][0];
 			}
 		}
 

application/tests/test_libraries_pygments.php
@@ -32,6 +32,10 @@ class test_libraries_pygments extends Test {
 		$p = new \libraries\Pygments('/invalid/filepath', 'application/x-php', 'stdin');
 		$this->t->is($p->autodetect_lexer(), 'php', "application/php should be php");
 
+		// This is from pygments and not our hardcoded list
+		$p = new \libraries\Pygments('/invalid/filepath', 'text/x-pascal', 'stdin');
+		$this->t->is($p->autodetect_lexer(), 'delphi', "text/x-pascal should be delphi");
+
 		$p = new \libraries\Pygments('/invalid/filepath', 'application/octet-stream', 'stdin');
 		$this->t->is($p->autodetect_lexer(), false, "application/octet-stream should return false");
     }

scripts/get_lexer_list.py
@@ -11,7 +11,7 @@ def dictify(list):
 for fullname, names, exts, mimetypes in pygments.lexers.get_all_lexers():
     ret.append({
         'fullname': fullname,
-        'names': dictify(names),
+        'names': names,
         'extentions': dictify(exts),
         'mimetypes': dictify(mimetypes),
         })

commit 84b8cf26000c8709c49ff8811dc6c7c34b1406f0

Fix missing lexers in get_lexers
Broken by a11e06d4906c2e1186779b42d5e87a17f66c50ea which changed names
back to a normal array.

Signed-off-by: Florian Pritz <bluewind@xinu.at>

application/libraries/Pygments.php
@@ -31,12 +31,12 @@ class Pygments {
 	}
 
 	public static function get_lexers() {
-		return cache_function('lexers', 1800, function() {
+		return cache_function('lexers-v2', 1800, function() {
 			$last_desc = "";
 
 			foreach (self::get_pygments_info() as $lexer) {
 				$desc = $lexer['fullname'];
-				$name = array_keys($lexer['names'])[0];
+				$name = $lexer['names'][0];
 				if ($desc == $last_desc) {
 					continue;
 				}