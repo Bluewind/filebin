55d7f35510ddc528423f07b1f164b3d2623a93f5
Joakim Reinert <mail@jreinert.com>
2015-05-22 09:58:52 +0200

    No need to remove main.min.*.js anymore. Relict from earlier stage.

--- scripts/optimize_js.sh
+++ scripts/optimize_js.sh
@@ -3,5 +3,4 @@
 script_dir=$(dirname "$0")
 js_dir="$script_dir/../data/js"
 outfile="$js_dir/main.min.js"
-rm -f "$js_dir"/main.min.*.js
 node "$script_dir/r.js" -o mainConfigFile="$js_dir/main.js" name=main out=$outfile
