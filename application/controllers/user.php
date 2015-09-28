5cc91f8cd4b9f1a3a2b17d5e5bf7606acf0fcd07
Florian Pritz <bluewind@xinu.at>
2015-09-28 17:15:12 +0200

    Fix login redirect with multipate URL
    The - from m-ID was missing in the regex.
    
    Signed-off-by: Florian Pritz <bluewind@xinu.at>

--- application/controllers/user.php
+++ application/controllers/user.php
@@ -47,7 +47,7 @@ class User extends MY_Controller {
 		$redirect_uri = $this->input->get("redirect_uri");
 		$this->muser->require_session();
 
-		if (!preg_match('/^[0-9a-zA-Z\/_]*$/', $redirect_uri)) {
+		if (!preg_match('/^[0-9a-zA-Z\/_-]*$/', $redirect_uri)) {
 			$redirect_uri = '/';
 		}
 
