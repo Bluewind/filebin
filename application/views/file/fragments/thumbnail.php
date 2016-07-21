0db79529d129dd4fe1e9b7bf823e07510c806bd4
Florian Pritz <bluewind@xinu.at>
2016-07-21 19:01:47 +0200

    history: Disable lazy loading for the first 42 thumbnails
    This speeds up the initial rendering of the page. For a long history
    (> ~1000 items) the lazyloading JS creates a noticeable delay (>500ms)
    depending on the cpu power of the machine.
    
    Disable lazy loading for the first items so that these are loaded
    normally by the browser parallel to the scripts execution.
    
    Signed-off-by: Florian Pritz <bluewind@xinu.at>

--- application/views/file/fragments/thumbnail.php
+++ application/views/file/fragments/thumbnail.php
@@ -5,9 +5,12 @@ $base_url = site_url();
 if (substr($base_url, -1) !== "/") {
 	$base_url .= "/";
 }
+$counter = 0;
 ?>
 <div class="upload_thumbnails"><!--
-	<?php foreach($items as $key => $item): ?>
+	<?php foreach($items as $key => $item):
+		$counter++;
+	 ?>
 		--><a
 			<?php if (strpos($item["mimetype"], "image/") === 0) {?>rel="gallery" class="colorbox"<?php } ?>
 			data-orientation="<?php echo $item["orientation"]; ?>"
@@ -15,9 +18,16 @@ if (substr($base_url, -1) !== "/") {
 			title="<?php echo htmlentities($item["filename"], ENT_QUOTES); ?>"
 			data-content="<?php echo htmlentities($item["tooltip"], ENT_QUOTES); ?>"
 			data-id="<?php echo $item["id"]; ?>"><!--
-			--><img	class="thumb lazyload"
-				data-original="<?php echo $base_url."file/thumbnail/".$item["id"]; ?>"
-				><!--
+			--><?php if ($counter > 42) {
+				?><img
+					class="thumb lazyload"
+					data-original="<?php echo $base_url."file/thumbnail/".$item["id"]; ?>"
+				><?php
+			} else {
+				 ?><img
+					class="thumb"
+					src="<?php echo $base_url."file/thumbnail/".$item["id"]; ?>"><?php
+			} ?><!--
 			--><noscript><img class="thumb" src="<?php echo $base_url."file/thumbnail/".$item["id"]; ?>"></noscript></a><!--
 	<?php endforeach; ?>
 	-->
