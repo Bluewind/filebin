1a39024190329a002be034d29979e1e8e5902607
Florian Pritz <bluewind@xinu.at>
2016-07-31 10:56:52 +0200

    Install git hooks as relative symlinks
    Absolute symlinks break when the installation is moved on the server and
    provide no value.
    
    Also update the post-merge hook so that it performs this change.
    
    Signed-off-by: Florian Pritz <bluewind@xinu.at>

--- scripts/install-git-hooks.sh
+++ scripts/install-git-hooks.sh
@@ -11,5 +11,5 @@ for hook in $HOOK_NAMES; do
     # create the symlink, overwriting the file if it exists
     # probably the only way this would happen is if you're using an old version of git
     # -- back when the sample hooks were not executable, instead of being named ____.sample
-    ln -s -f $SCRIPTS_DIR/hooks-wrapper.sh $HOOK_DIR/$hook
+    ln -r -s -f $SCRIPTS_DIR/hooks-wrapper.sh $HOOK_DIR/$hook
 done
