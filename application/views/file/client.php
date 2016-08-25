d3301dbfd99eb7ea1c0f99a50ba075142b2392a2
Florian Pritz <bluewind@xinu.at>
2016-08-25 16:33:30 +0200

    Remove documentation for deprecated API
    Signed-off-by: Florian Pritz <bluewind@xinu.at>

--- application/views/file/client.php
+++ application/views/file/client.php
@@ -6,27 +6,6 @@
 	GPG sigs, older versions: <?php echo anchor("https://paste.xinu.at/data/client"); ?>
 </p>
 
-<p>To authenticate add the following to your ~/.netrc:</p>
-
-<pre>
-machine <?php echo $domain; ?> login my_username password my_secret_password
-</pre>
-
-<p>
-	If you are using fb-client &ge;1.2 you can
-	<a href="<?php echo	site_url("user/apikeys"); ?>">create an API key</a>,
-	save it in <code>~/.config/fb-client/apikey</code> and remove
-	your password from <code>.netrc</code>. Please refer to <code>man
-	1 fb</code> for further details.
-</p>
-
-<p>
-	If you are using fb-client &ge;1.1 you can use
-	<code>~/.config/fb-client/config</code> to upload to a different
-	pastebin URL (https or you own installation). Please refer to
-	<code>man 1 fb</code> for further details.
-</p>
-
 <h2>Linux</h2>
 <p>
 	Arch Linux: pacman -S fb-client<br />
@@ -40,11 +19,3 @@ machine <?php echo $domain; ?> login my_username password my_secret_password
 <p>
 	Get <a href="http://brew.sh">Homebrew</a> and run <code>brew install fb-client</code>.
 </p>
-
-<h1>Shell</h1>
-
-<pre>
-curl -n -F "file=@/home/user/foo" <?php echo site_url("file/do_upload"); ?>   (binary safe)
-cat file | curl -n -F "file=@-;filename=stdin" <?php echo site_url("file/do_upload"); ?>   (binary safe)
-</pre>
-
--- application/views/file_plaintext/client.php
+++ application/views/file_plaintext/client.php
@@ -1,12 +1,4 @@
-Shell (binary safe):
-  curl -n -F "file=@/home/user/foo" <?php echo site_url("file/do_upload")."\n"; ?>
-  cat file | curl -n -F "file=@-;filename=stdin" <?php echo site_url("file/do_upload")."\n"; ?>
-
 Client:
 Development (git): http://git.server-speed.net/users/flo/fb
 Latest release: <?php echo $client_link."\n"; ?>
 GPG sigs, older versions: https://paste.xinu.at/data/client
-
-To authenticate add the following to your ~/.netrc:
-  machine paste.xinu.at login my_username password my_secret_password
-
