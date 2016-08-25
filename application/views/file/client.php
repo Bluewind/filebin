bcfcecf600fe033c925dd42d358b4c086d3a4a10
Florian Pritz <bluewind@xinu.at>
2016-08-25 16:35:46 +0200

    Drop links to outdated clients
    These all build fb-client <2.0 and therefore use the deprecated API.
    
    Signed-off-by: Florian Pritz <bluewind@xinu.at>

--- application/views/file/client.php
+++ application/views/file/client.php
@@ -9,13 +9,6 @@
 <h2>Linux</h2>
 <p>
 	Arch Linux: pacman -S fb-client<br />
-	Debian: <?php echo anchor("https://paste.xinu.at/data/client/deb"); ?><br />
 	Gentoo: Add <a href="https://git.holgersson.xyz/holgersson-overlay/tree/README">this overlay</a> and run <code>emerge -a fb-client</code><br />
-	Slackware: <?php echo anchor("https://paste.xinu.at/data/client/slackware"); ?><br />
-	OpenSUSE: <?php echo anchor("https://build.opensuse.org/package/show/home:mwilhelmy/fb-client"); ?>
 </p>
 
-<h2>OS X</h2>
-<p>
-	Get <a href="http://brew.sh">Homebrew</a> and run <code>brew install fb-client</code>.
-</p>
