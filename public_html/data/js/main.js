4df1aac4b7563d73baa210616a39bafe1b9590f0
Florian Pritz <bluewind@xinu.at>
2016-08-28 10:52:23 +0200

    Update jQuery UI and include sortable
    Also drop in all the non minified version because we can and it might
    be useful for debugging in the future.
    
    Signed-off-by: Florian Pritz <bluewind@xinu.at>

--- public_html/data/js/main.js
+++ public_html/data/js/main.js
@@ -11,7 +11,7 @@ requirejs.config({
 	},
 	paths: {
 		'jquery': 'vendor/jquery-2.0.3.min',
-		'jquery-ui': 'vendor/jquery-ui-1.10.3.custom.min',
+		'jquery-ui': 'vendor/jquery-ui.min',
 		'bootstrap': 'vendor/bootstrap.min',
 		'jquery.tablesorter': 'vendor/jquery.tablesorter.min',
 		'jquery.lazyload': 'vendor/jquery.lazyload',
