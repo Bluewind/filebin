53c8126583f33141014d0f86104637168571b89d
Florian Pritz <bluewind@xinu.at>
2016-09-26 11:49:15 +0200

    Simplify error page for cli requests
    Signed-off-by: Florian Pritz <bluewind@xinu.at>

--- application/errors/error_general.php
+++ application/errors/error_general.php
@@ -11,11 +11,7 @@ if (class_exists("CI_Controller") && !isset($GLOBALS["is_error_page"])) {
 	$CI->load->helper("filebin");
 	$CI->load->helper("url");
 
-	if ($CI->input->is_cli_request()) {
-		is_cli_client(true);
-	}
-
-	if (is_cli_client()) {
+	if (is_cli_client() || $CI->input->is_cli_request()) {
 		$message = str_replace("</p>", "</p>\n", $message);
 		$message = strip_tags($message);
 		echo "$heading: $message\n";
