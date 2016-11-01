Bluewind/filebin

commit d2fdfc77f7a9485548869414d4377cd6848fe339

Improve coding style for return statements that only check one condition
Signed-off-by: Florian Pritz <bluewind@xinu.at>

application/models/mmultipaste.php
@@ -70,11 +70,7 @@ class Mmultipaste extends CI_Model {
 			LIMIT 1';
 		$query = $this->db->query($sql, array($id));
 
-		if ($query->num_rows() == 1) {
-			return true;
-		} else {
-			return false;
-		}
+		return $query->num_rows() == 1;
 	}
 
 	public function valid_id($id)
@@ -129,11 +125,7 @@ class Mmultipaste extends CI_Model {
 		$f = new \service\storage($this->get_tarball_path($id));
 		$f->unlink();
 
-		if ($this->id_exists($id))  {
-			return false;
-		}
-
-		return true;
+		return $this->id_exists($id);
 	}
 
 	public function get_owner($id)

application/models/mfile.php
@@ -64,11 +64,7 @@ class Mfile extends CI_Model {
 			->limit(1)
 			->get();
 
-		if ($query->num_rows() == 1) {
-			return true;
-		} else {
-			return false;
-		}
+		return $query->num_rows() == 1;
 	}
 
 	function get_filedata($id)
@@ -195,11 +191,7 @@ class Mfile extends CI_Model {
 			->limit(1)
 			->get();
 
-		if ($query->num_rows() == 0) {
-			return true;
-		} else {
-			return false;
-		}
+		return $query->num_rows() == 0;
 	}
 
 	public function delete_by_user($userid)

commit ecb9a19cfaf627af4fc316a2125cd10d7e233150

mmultipaste->delete_id: Fix inverted return value
Broken in d2fdfc77f7a9485548869414d4377cd6848fe339

Signed-off-by: Florian Pritz <bluewind@xinu.at>

application/models/mmultipaste.php
@@ -125,7 +125,7 @@ class Mmultipaste extends CI_Model {
 		$f = new \service\storage($this->get_tarball_path($id));
 		$f->unlink();
 
-		return $this->id_exists($id);
+		return !$this->id_exists($id);
 	}
 
 	public function get_owner($id)

application/test/tests/test_api_v2.php
@@ -494,4 +494,33 @@ class test_api_v2 extends \test\Test {
 			),
 		), $ret, "expected error response");
 	}
+
+	public function test_delete_canDeleteMultipaste()
+	{
+		$apikey = $this->createUserAndApikey();
+		$ret = $this->uploadFile($apikey, "data/tests/small-file");
+		$id = $ret["data"]["ids"][0];
+		$ret = $this->CallEndpoint("POST", "file/create_multipaste", array(
+			"apikey" => $apikey,
+			"ids[1]" => $id,
+		));
+		$this->expectSuccess("create multipaste", $ret);
+
+		$mid = $ret['data']['url_id'];
+		$ret = $this->CallEndpoint("POST", "file/delete", array(
+			"apikey" => $apikey,
+			"ids[1]" => $mid,
+		));
+		$this->expectSuccess("delete uploaded file", $ret);
+
+		$this->t->ok(empty($ret["data"]["errors"]), "no errors");
+		$this->t->is_deeply(array(
+			$mid => array(
+				"id" => $mid
+			)
+		), $ret["data"]["deleted"], "deleted wanted ID");
+		$this->t->is($ret["data"]["total_count"], 1, "total_count correct");
+		$this->t->is($ret["data"]["deleted_count"], 1, "deleted_count correct");
+	}
+
 }