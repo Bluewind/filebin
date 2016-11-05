Bluewind/filebin

commit 9efd2db81ccf987a5455e0e4575c7d3f9072870f

scripts/install-git-hooks.sh: Drop ln -r to support busybox ln
Signed-off-by: Florian Pritz <bluewind@xinu.at>

scripts/install-git-hooks.sh
@@ -14,5 +14,5 @@ for hook in $HOOK_NAMES; do
     # create the symlink, overwriting the file if it exists
     # probably the only way this would happen is if you're using an old version of git
     # -- back when the sample hooks were not executable, instead of being named ____.sample
-    ln -r -s -f $SCRIPTS_DIR/hooks-wrapper.sh $HOOK_DIR/$hook
+    ln -f ../../scripts/hooks-wrapper.sh $HOOK_DIR/$hook
 done

commit 6b9278cd471a0d4c6140069c765662a5ea1de838

install-git-hooks.sh: Add missing -s to ln
Broken in 9efd2db81ccf987a5455e0e4575c7d3f9072870f

Signed-off-by: Florian Pritz <bluewind@xinu.at>

scripts/install-git-hooks.sh
@@ -14,5 +14,5 @@ for hook in $HOOK_NAMES; do
     # create the symlink, overwriting the file if it exists
     # probably the only way this would happen is if you're using an old version of git
     # -- back when the sample hooks were not executable, instead of being named ____.sample
-    ln -f ../../scripts/hooks-wrapper.sh $HOOK_DIR/$hook
+    ln -s -f ../../scripts/hooks-wrapper.sh $HOOK_DIR/$hook
 done