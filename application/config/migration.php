1846a9c0fae6541b0eab130a506e9c5ce18c2d7f
Florian Pritz <bluewind@xinu.at>
2017-09-16 14:28:30 +0200

    Update ci_session table for CI3
    Signed-off-by: Florian Pritz <bluewind@xinu.at>

--- application/config/migration.php
+++ application/config/migration.php
@@ -69,7 +69,7 @@ $config['migration_auto_latest'] = FALSE;
 | be upgraded / downgraded to.
 |
 */
-$config['migration_version'] = 19;
+$config['migration_version'] = 20;
 
 /*
 |--------------------------------------------------------------------------
---
+++ application/migrations/020_update_session_table.php
@@ -0,0 +1,42 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Migration_update_session_table extends CI_Migration {
+
+	public function up()
+	{
+		$prefix = $this->db->dbprefix;
+
+		if ($this->db->dbdriver == 'postgre') {
+			$this->db->query('
+				ALTER TABLE "'.$prefix.'ci_sessions"
+					DROP COLUMN "user_agent";
+				');
+			$this->db->query('
+				ALTER TABLE "'.$prefix.'ci_sessions"
+					RENAME COLUMN "session_id" TO "id";
+				');
+			$this->db->query('
+				ALTER TABLE "'.$prefix.'ci_sessions"
+					RENAME COLUMN "last_activity" TO "timestamp";
+				');
+			$this->db->query('
+				ALTER TABLE "'.$prefix.'ci_sessions"
+					RENAME COLUMN "user_data" TO "data";
+				');
+		} else {
+			$this->db->query('
+				ALTER TABLE `'.$prefix.'ci_sessions`
+					DROP `user_agent`,
+					CHANGE `session_id` `id` VARCHAR(40) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT 0,
+					CHANGE `last_activity` `timestamp` INT(10) UNSIGNED NOT NULL DEFAULT 0,
+					CHANGE `user_data` `data` BLOB NOT NULL;
+				');
+		}
+	}
+
+	public function down()
+	{
+		throw new \exceptions\ApiException("migration/downgrade-not-supported", "downgrade not supported");
+	}
+}
